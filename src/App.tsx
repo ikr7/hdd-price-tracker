504a5661dfec5a2dfa77a27842877f71487bcad0
ikr7 <kunpei.ikuta@gmail.com>
2022-06-02 13:22:26 +0900

    iroiro

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import { useEffect, useState } from 'react';
 
-import { Table, PageHeader } from 'antd';
-import type { ColumnsType } from 'antd/lib/table';
+import { Table, PageHeader, Tooltip } from 'antd';
+import type { ColumnsType, ColumnType } from 'antd/lib/table';
 
 import 'antd/dist/antd.css';
-import { GithubOutlined } from '@ant-design/icons';
+import { GithubOutlined, StarFilled } from '@ant-design/icons';
 
 type HDDEntry = {
   id: number;
@@ -43,106 +43,127 @@ function App() {
     })();
   }, []);
 
+  function unitCapacity(capacity: number) {
+    return capacity >= 1 ? `${capacity} TB` : `${capacity * 1000} GB`
+  }
+
+  function formatPrice(x?: number) {
+    return x ? '\xa5' + x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") : null;
+  }
+
+  type ShopName = 'amazonjp' | 'tsukumo' | 'sofmap' | 'pckoubou' | 'dospara';
+
+  function priceColumn(title: string, shopName: ShopName): ColumnType<HDDEntry> {
+    return {
+      dataIndex: ['prices', shopName],
+      title: title,
+      render: (price, record) => {
+        const isLowest = price === Math.min(...Object.values(record.prices).filter(price => price));
+        return (
+          <Tooltip title={isLowest ? 'lowest price' : ''}>
+            <a
+              href={record.links[shopName]}
+              target='_blank'
+              rel='noreferrer'
+            >
+              {formatPrice(price)} {isLowest ? <StarFilled /> : null}
+            </a>
+          </Tooltip>
+        )
+      }
+    }
+  }
+
   const columns: ColumnsType<HDDEntry> = [
     {
       title: 'Manufacturer',
       dataIndex: 'manufacturer',
-      key: 'manufacturer'
+      filters: Array.from(new Set(rows.map(row => row.manufacturer))).map(value => {
+        return {
+          text: value,
+          value: value
+        };
+      }),
+      onFilter: (value, record) => record.manufacturer === value
     },
     {
       title: 'Brand',
       dataIndex: 'brand',
-      key: 'brand'
+      filters: Array.from(new Set(rows.map(row => row.brand))).map(value => {
+        return {
+          text: value,
+          value: value
+        };
+      }),
+      onFilter: (value, record) => record.brand === value
     },
     {
-      title: 'capacity',
+      title: 'Capacity',
       dataIndex: 'capacity',
-      key: 'capacity'
+      filters: Array.from(new Set(rows.map(row => row.capacity))).sort().map(value => {
+        return {
+          text: unitCapacity(value),
+          value: value
+        };
+      }),
+      onFilter: (value, record) => record.capacity === value,
+      render: (text, record) => unitCapacity(record.capacity)
     },
     {
-      title: 'rpm',
+      title: 'RPM',
       dataIndex: 'rpm',
-      key: 'rpm'
+      filters: Array.from(new Set(rows.map(row => row.rpm))).sort().map(value => {
+        return {
+          text: value,
+          value: value
+        };
+      }),
+      onFilter: (value, record) => record.rpm === value
     },
     {
-      title: 'record',
+      title: 'Record',
       dataIndex: 'record',
-      key: 'record'
+      filters: Array.from(new Set(rows.map(row => row.record))).map(value => {
+        return {
+          text: value,
+          value: value
+        };
+      }),
+      onFilter: (value, record) => record.record === value
     },
     {
-      title: 'cache',
+      title: 'Cache',
       dataIndex: 'cache',
-      key: 'cache'
+      render: (text, record) => `${record.cache} MB`,
+      sorter: (a, b) => a.cache - b.cache
     },
     {
-      title: 'model',
+      title: 'Model',
       dataIndex: 'model',
-      key: 'model'
     },
     {
-      dataIndex: ['prices', 'tsukumo'],
-      title: 'tsukumoPrice',
-      key: 'tsukumoPrice',
-      render: (price, row) => {
-        return (
-          <a
-            href={row.links.tsukumo}
-            target='_blank'
-            rel='noreferrer'
-          >
-              {price}
-          </a>
-        )
-      }
-    },
-    {
-      dataIndex: ['prices', 'sofmap'],
-      title: 'sofmapPrice',
-      key: 'sofmapPrice',
-      render: (price, row) => {
-        return (
-          <a
-            href={row.links.sofmap}
-            target='_blank'
-            rel='noreferrer'
-          >
-              {price}
-          </a>
-        )
-      }
-    },
-    {
-      dataIndex: ['prices', 'pckoubou'],
-      title: 'pckoubouPrice',
-      key: 'pckoubouPrice',
-      render: (price, row) => {
-        return (
-          <a
-            href={row.links.pckoubou}
-            target='_blank'
-            rel='noreferrer'
-          >
-              {price}
-          </a>
-        )
-      }
-    },
-    {
-      dataIndex: ['prices', 'dospara'],
-      title: 'dosparaPrice',
-      key: 'dosparaPrice',
-      render: (price, row) => {
-        return (
-          <a
-            href={row.links.dospara}
-            target='_blank'
-            rel='noreferrer'
-          >
-              {price}
-          </a>
-        )
-      }
-    },
+      title: 'Prices',
+      key: 'prices',
+      sorter: (a, b) => {
+        const aPrices = Object.values(a.prices).filter(price => price);
+        const bPrices = Object.values(b.prices).filter(price => price);
+        return Math.max(...aPrices) - Math.max(...bPrices);
+      },
+      filters: [
+        {
+          text: 'hide if no price data available',
+          value: 'hide_no_price'
+        }
+      ],
+      defaultFilteredValue: ['hide_no_price'],
+      onFilter: (value, record) => Object.values(record.prices).filter(price => price).length !== 0,
+      children: [
+        priceColumn('Tsukumo', 'tsukumo'),
+        priceColumn('Sofmap', 'sofmap'),
+        priceColumn('PC-Koubou', 'pckoubou'),
+        priceColumn('Dospara', 'dospara'),
+      ]
+    }
   ];
 
   return (
@@ -151,7 +172,10 @@ function App() {
       title="HDD Price Tracker"
       backIcon={false}
       extra={[
-        <a href="https://github.com/ikr7/hdd-price-tracker">
+        <a
+          href="https://github.com/ikr7/hdd-price-tracker"
+          key='github'
+        >
           <GithubOutlined />
         </a>
       ]}
@@ -164,6 +188,7 @@ function App() {
         pageSize: 1000,
         position: []
       }}
+      rowKey="id"
     />
   </>
   );
